b375565b9a34ca4bdf3a7a550ac9f18b2250c8e7
ElEeMeNT <ali.pourrashidi.j@gmail.com>
2024-10-11 13:17:00 +0330

    Fix Team

--- basicESP/Program.cs
+++ basicESP/Program.cs
@@ -14,9 +14,9 @@ Vector2 screenSize = renderer.screensize;
 List<Entity> entities = new List<Entity>();
 Entity localPlayer = new Entity();
 
-int dwEntityList = 0x19BDCF0;
-int dwViewMatrix = 0x1A1FF70;
-int dwLocalPlayerPawn = 0x1825158;
+int dwEntityList = 0x19BDE30;
+int dwViewMatrix = 0x1A1FF40;
+int dwLocalPlayerPawn = 0x1825138;
 
 int m_vOldOrigin = 0x131C;
 int m_iTeamNum = 0x3E3;
@@ -24,6 +24,11 @@ int m_lifeState = 0x348;
 int m_hPlayerPawn = 0x80C;
 int m_vecViewOffset = 0xCA8;
 int m_iszPlayerName = 0x660;
+
+int m_Item = 0x50;
+int m_pClippingWeapon = 0x1398;
+int m_iItemDefinitionIndex = 0x1BA;
+int m_AttributeManager = 0x1140;
 
 int m_iHealth = 0x344;
 while (true)
@@ -45,8 +50,11 @@ while (true)
         if (currentPawn == IntPtr.Zero) continue;
 
         int lifeState = swed.ReadInt(currentPawn, m_lifeState);
-
         if (lifeState != 256) continue;
+        IntPtr currentWeapon = swed.ReadPointer(currentPawn, m_pClippingWeapon);
+        short WeaponDefinitionIndex = swed.ReadShort(currentWeapon, m_AttributeManager + m_Item + m_iItemDefinitionIndex);
+        if (WeaponDefinitionIndex == -1) continue;
+        Console.WriteLine(currentWeapon);
 
         float heightOffset = 12;
         float[] viewMatrix = swed.ReadMatrix(client + dwViewMatrix);
@@ -57,13 +65,16 @@ while (true)
         entity.viewoffset = swed.ReadVec(currentPawn, m_vecViewOffset);
         Vector3 aboveHeadPosition = new Vector3(entity.position.X, entity.position.Y, entity.position.Z + heightOffset);
         entity.position2D = Calculate.WorldToScreen(viewMatrix, entity.position, screenSize);
+        entity.currentWeaponName = Enum.GetName(typeof(Weapon), WeaponDefinitionIndex);
         entity.name = swed.ReadString(currentController, m_iszPlayerName, 16).Split("\0")[0];
         entity.viewPosition2D = Calculate.WorldToScreen(viewMatrix, Vector3.Add(aboveHeadPosition, entity.viewoffset), screenSize);
         entities.Add(entity);
+        Console.WriteLine($"ent : {entity.currentWeaponName}");
 
 
     }
     renderer.UpdateLocalPlayer(localPlayer);
     renderer.UpdateEntites(entities);
-
+    Thread.Sleep(5);
+    Console.Clear();
 }
